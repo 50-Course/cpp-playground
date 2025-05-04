3dc50fb0297bf262b5add0fdcdcba53cb455561a
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-05-04 01:11:42 +0100

    playing around with Cpp bringing my experience from other languages

---
+++ main.cpp
@@ -0,0 +1,37 @@
+// the include delecrate is the import-like statement for working modules
+#include <iostream>
+#include <string>
+
+// WAIT A DAMN MINUTE? WHY DO I HAVE TO MOVE THIS CODE BLOCK UP HERE?
+//
+// SO THIS SHIT RESPECT ORDER OF EXECUTION, MID :puke:
+void learnTypes() {
+  // data types
+
+  int x; // should default nullptr, i believe or null or kind of
+  char y = 'A';
+  std::string name = "50-Course"; // ah intersting, so i need to import the
+                                  // string module from stdlib to use strings?
+
+  std::cout << name << std::endl;
+}
+
+// so the sample Hello World program
+//
+// it seems when we want to define a function
+// its similar to c-based languages
+//
+// <type> funcName(func-signature)
+//
+// what i don't know yet, by default is defined functions, public or
+// protected?
+int main() {
+
+  // cout is the standard output PrintStream
+  std::cout << "Hello, World!" << std::endl;
+  learnTypes();
+
+  // every c progaram must have an exit status
+  // 0 here means success
+  return 0;
+}
